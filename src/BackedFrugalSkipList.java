2eeb4b4b9fb108f16484c94da3814196c7a871fd
uzayhazar <hazarulas9@gmail.com>
2025-11-19 23:12:40 +0100

    Finished Task 1.2

---
+++ src/BackedFrugalSkipList.java
@@ -0,0 +1,56 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import provided_classes.KVStore;
+import provided_classes.VersionList;
+import provided_classes.Serializer;
+
+public class BackedFrugalSkipList<P> implements VersionList<P>, Serializer<P> {
+    private final FrugalSkipList<P> list;
+    private final KVStore store;
+    private final ObjectMapper objectMapper;
+
+    public BackedFrugalSkipList(KVStore store) {
+        this.store = store;
+        this.list = new FrugalSkipList<>();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public void append(P p, long timestamp) {
+        list.append(p, timestamp);
+        store.put(String.valueOf(timestamp), serialize(p));
+    }
+
+    @Override
+    public P findVisible(long timestamp) {
+        P result = list.findVisible(timestamp);
+        if (result != null) {
+            return deSerialize(store.get(String.valueOf(timestamp)));
+        }
+        return null;
+    }
+
+    @Override
+    public String serialize(P p) {
+        if (p != null) {
+            try {
+                return objectMapper.writeValueAsString(p);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return "";
+    }
+
+    @Override
+    public P deSerialize(String serializedT) {
+        if (serializedT != null && !serializedT.isEmpty()) {
+            try {
+                return objectMapper.readValue(serializedT, (Class<P>) Object.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
+}
