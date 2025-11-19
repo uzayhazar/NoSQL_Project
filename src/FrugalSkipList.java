264cf1bc520b0a449fad03c59411691ed9cb7fba
uzayhazar <hazarulas9@gmail.com>
2025-11-19 23:05:51 +0100

    Update FrugalSkipList.java
    Finished task 1.2

--- src/FrugalSkipList.java
+++ src/FrugalSkipList.java
@@ -1,27 +1,53 @@
 import provided_classes.VersionList;
 
+import java.util.Random;
 public class FrugalSkipList<P>  implements VersionList<P> {
 
+    private FSListNode head;
+    private final Random random = new Random();
     class FSListNode {
         long timestamp;
-        Object payload;
+        P payload;
         FSListNode next;
         FSListNode ridgy;
         int level;
 
-        FSListNode(long timestamp, Object payload) {
+        FSListNode(long timestamp, P payload) {
             this.timestamp = timestamp;
             this.payload = payload;
             this.level = 0;
         }
     }
     @Override
-    public void append(Object o, long timestamp) {
+    public void append(P o, long timestamp) {
+        FSListNode newVersion = new FSListNode(timestamp, o);
+        newVersion.next = head;
+        if(head != null && random.nextBoolean()) {
+            newVersion.level = head.level + 1;
+        }
+        else {
+            newVersion.level = 0;
+        }
 
+        FSListNode current = head;
+        while (current != null && current.level < newVersion.level) {
+            current = current.ridgy;
+        }
+        newVersion.ridgy = current;
+        head = newVersion;
     }
 
     @Override
     public P findVisible(long timestamp) {
-        return null;
+        FSListNode current = head;
+        while (current != null && current.timestamp > timestamp) {
+
+            if (current.ridgy != null && current.ridgy.timestamp > timestamp) {
+                current = current.ridgy;
+            } else {
+                current = current.next;
+            }
+        }
+        return current != null ? current.payload : null;
     }
 }
